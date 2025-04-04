276b377323e692bf464b98ffb5d8e0c7c1a1f0da
SD <serge.delbosc@outlook.fr>
2025-04-04 11:43:51 +0200

    Premier test par l'url

---
+++ ControleDocker.ps1
@@ -0,0 +1,5 @@
+﻿$response = Invoke-WebRequest -Uri http://127.0.0.1:2375/info
+$data = $response.Content | ConvertFrom-Json
+
+Write-Host "Containers : $($data.Containers)" -ForegroundColor Green
+Write-Host "ContainersRunning  : $($data.ContainersRunning )" -ForegroundColor Green
